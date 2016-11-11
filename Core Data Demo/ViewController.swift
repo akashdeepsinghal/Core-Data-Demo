71bd35be69527bf5603548056d991891c209923e
akigupta131 <akigupta131@gmail.com>
2016-11-11 17:59:17 +0530

    Tested with predicates for filtering out results

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -20,9 +20,9 @@ class ViewController: UIViewController {
         
         let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
         
-        newUser.setValue("Akash", forKey: "username")
+        newUser.setValue("Prateek", forKey: "username")
         newUser.setValue("myPass", forKey: "password")
-        newUser.setValue(23, forKey: "age")
+        newUser.setValue(37, forKey: "age")
         
         do {
             try context.save()
@@ -31,7 +31,37 @@ class ViewController: UIViewController {
         } catch {
             print("There was some error")
         }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+//        request.predicate = NSPredicate(format: "age = %@", "10")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if (result.value(forKey: "username") as? String) != nil {
+                        
+//                        result.setValue("Dooley", forKey: "username")
+//
+//                        do {
+//                            try context.save()
+//                        } catch {
+//                            print("Error saving context")
+//                        }
+                        if let username = result.value(forKey: "username") {
+                            print(username)
+                        }
+                    }
+                }
+            } else {
+                print("No result found")
+            }
+        } catch {
+            print("Couldn't fetch result")
+        }
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
